01ebfc590c1a7710380116094da7f811ccfed7f2
Natan Zamansky <natan.zamansky@gmail.com>
2014-11-11 14:19:54 -0500

    We can now sort of search for combinations of tags, resulting in far fewer blank searches. It's still not perfect, but a step. Also, the headline search has been expanded to 50 headlines, and the 10 most common words.

--- app.py
+++ app.py
@@ -3,36 +3,66 @@ import urllib2, json, spotipy, sys, praw
 
 app=Flask(__name__)
 
+def getCombinations(tags):
+    #This function does not actually return all possible combinations of tags. For our purposes, though, it's good enough.
+    combs=[]
+    size=len(tags)
+    index=0
+    while size>0:
+        while index<=len(tags)-size:
+            new=[]
+            for i in range(index,size):
+                new.append(tags[i])
+            combs.append(new)
+            index=index+1
+        index=0
+        size=size-1
+    return combs
+
 def getRecipes(tags):
     yummly = "http://api.yummly.com/v1/api/recipes?_app_id=4a5d0d78&_app_key=818e8b8a15bd453e736c1308a331d7f8&q="
-    for tag in tags:
-        yummly+=tag+"+"
-        request = urllib2.urlopen(yummly)
-        d=json.loads(request.read())
+    tags=getCombinations(tags)
+    i=0
     results=[]
-    for r in d['matches']:
-        results.append(r['recipeName']+": "+str(r['totalTimeInSeconds']))
+    while len(results)==0:
+        for tag in tags[i]:
+            yummly+=tag+"+"
+            request = urllib2.urlopen(yummly)
+            d=json.loads(request.read())
+ 
+        results=[]
+        for r in d['matches']:
+            results.append(r['recipeName']+": "+str(r['totalTimeInSeconds']))
+        i=i+1
     return results
 
 #print getRecipes(["of"])
 
 def getSongs(tag):
     sp = spotipy.Spotify()
-    songs = sp.search(q='track:' + tag, type='track')
-    items = songs['tracks']['items']
-    i = 0
-    results = []
-    for item in items:
-        track = items[i]
-        results.append( track['name'] )
-        i = i + 1
+    tags=tag.split()
+    tags=getCombinations(tags)
+    results=[]
+    i=0
+    while len(results)==0:
+        src=""
+        for tag in tags[i]:
+            src=src+tag+"+"
+        src=src[:-1]
+        songs = sp.search(q='track:' + src, type='track')
+        items = songs['tracks']['items']
+        j = 0
+        results = []
+        for item in items:
+            track = items[i]
+            results.append( track['name'] )
+            j = j + 1
+        i=i+1
     return results
     
-#print getSongs("Chicken Caesar Pita")[0]
-
 def getHeadlines():
     r = praw.Reddit(user_agent='project-that-uses-a-lot-of-data-to-do-nothing')
-    headlines = r.get_subreddit('news').get_new(limit=10)
+    headlines = r.get_subreddit('news').get_new(limit=50)
     return [str(x) for x in headlines]
 
 headlines =  getHeadlines()
@@ -54,9 +84,12 @@ freq = []
 for w in sorted(words, key=words.get, reverse=True):
     freq.append([w,words[w]])
 
-print freq[0][0]
+topWords=[]
+for i in range(10):
+    topWords.append(freq[i][0])
+print topWords
 
-recipe = getRecipes([freq[0][0]])
+recipe = getRecipes(topWords)
 
 print recipe[0]
 
