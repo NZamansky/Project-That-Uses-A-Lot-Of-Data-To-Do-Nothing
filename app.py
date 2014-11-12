4374a4a2c81a2cf7ccef874a8ed36ff8079652be
ryustegolob <ryustegolob@gmail.com>
2014-11-11 20:40:44 -0500

    running into some errors with the yummly stuff

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def getCombinations(tags):
         size=size-1
     return combs
 
+#Gets all the recepes corresponding to a set of tags
 def getRecipes(tags):
     yummly = "http://api.yummly.com/v1/api/recipes?_app_id=4a5d0d78&_app_key=818e8b8a15bd453e736c1308a331d7f8&q="
     tags=getCombinations(tags)
@@ -34,8 +35,7 @@ def getRecipes(tags):
         i=i+1
     return results
 
-#print getRecipes(["of"])
-
+#Gets the songs corresponding to a certain tags
 def getSongs(tag):
     sp = spotipy.Spotify()
     tags=tag.split()
@@ -50,7 +50,6 @@ def getSongs(tag):
         songs = sp.search(q='track:' + src, type='track')
         items = songs['tracks']['items']
         j = 0
-        results = []
         for item in items:
             track = items[i]
             results.append( track['name'] )
@@ -58,45 +57,39 @@ def getSongs(tag):
         i=i+1
     return results
     
+#Gets the top words in the headlines
 def getHeadlines():
     r = praw.Reddit(user_agent='project-that-uses-a-lot-of-data-to-do-nothing')
     headlines = r.get_subreddit('news').get_new(limit=50)
-    return [str(x) for x in headlines]
+    headlines= [str(x) for x in headlines]
+    words = {}
+    for line in headlines: 
+        w = line.translate(None,"""1234567890,./;'[]\-=`~!@#$%^&*()_+{}|:"<>?""").split(' ')
+        for word in w:
+            if word not in words:
+                words[word]=1
+            else:
+                words[word]=words[word]+1    
+    freq = []
+    for w in sorted(words, key=words.get, reverse=True):
+        freq.append([w,words[w]])
+    topWords=[]
+    for i in range(10):
+        topWords.append(freq[i][0])
+    return topWords
 
-headlines =  getHeadlines()
 
-words = {}
+def allTogetherNow():
+    headlines = getHeadlines()
+    recipes = getRecipes(headlines)
+    song = getSongs(recipe[0].split(':')[0])
+    return song
 
-for line in headlines:
-    w = line.translate(None,"""1234567890,./;'[]\-=`~!@#$%^&*()_+{}|:"<>?""").split(' ')
-    for word in w:
-        if word not in words:
-            words[word]=1
-        else:
-            words[word]=words[word]+1
 
-# print words
-    
-freq = []
-
-for w in sorted(words, key=words.get, reverse=True):
-    freq.append([w,words[w]])
-
-topWords=[]
-for i in range(10):
-    topWords.append(freq[i][0])
-print topWords
-
-recipe = getRecipes(topWords)
-
-print recipe[0]
-
-songs = getSongs(recipe[0].split(':')[0])
-
-print songs
+print allTogetherNow()
 
 ########## webapp stuff ############
-
+'''
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
@@ -106,3 +99,4 @@ def base():
 if __name__=="__main__":
     app.debug=True
     app.run()
+'''
